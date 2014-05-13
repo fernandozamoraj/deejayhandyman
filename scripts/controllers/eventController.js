e81a0f4f919f7636bfc6789f1daf220a56ea2bf0
unknown <FernandoZ@OPS22W7.MAT.mclane.local>
2014-05-13 15:03:20 -0500

    added sorting on events

--- scripts/controllers/eventController.js
+++ scripts/controllers/eventController.js
@@ -1,5 +1,6 @@
 handymanApp.controller('eventController', function eventController($scope, eventsDataService){
     eventsDataService.getAll(function(events){
         $scope.events = events;
+
     });
 });
